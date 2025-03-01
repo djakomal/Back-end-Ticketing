3674a7ef95ae005ff31abb1859e33412ed6002c9
rafiou <rafiou.djako@ipnetinstitute.com>
2025-03-01 01:11:04 +0000

    Sauvegarde des modifications locales

---
+++ src/main/java/com/Personnel/Ticketing/Services/UtilisateurService.java
@@ -0,0 +1,10 @@
+package com.Personnel.Ticketing.Services;
+
+
+import com.Personnel.Ticketing.Models.Utilisateur;
+
+public interface UtilisateurService {
+
+
+    void inscription(Utilisateur utilisateur);
+}
